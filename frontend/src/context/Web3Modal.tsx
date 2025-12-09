dd108b373229903432c10edea1e840b8940617eb
Steven Elrod <your@email.com>
2025-12-08 22:54:33 -0600

    Add featured wallets to improve wallet selection UX

--- frontend/src/context/Web3Modal.tsx
+++ frontend/src/context/Web3Modal.tsx
@@ -18,8 +18,13 @@ if (!projectId) throw new Error("Project ID is not defined");
 createWeb3Modal({
   wagmiConfig: config,
   projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true, // Optional - false as default
+  enableAnalytics: true,
+  enableOnramp: true,
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase Wallet
+  ],
 });
 
 export default function Web3ModalProvider({
